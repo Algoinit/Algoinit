d1e822a46553f54b7aab0a75759399c93eeee46e
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-20 20:47:41 +0900

    [new] 프로그래머스 올바른 괄호 문제 풀이

--- Stack&Queue/Seunghwan/P12909.java
+++ Stack&Queue/Seunghwan/P12909.java
@@ -1,5 +1,18 @@
-package Hash.basicExample.baekjoon7785;
+import java.util.Stack;
 
 public class P12909 {
+    public boolean correctBrackets(String s) {
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if(stack.isEmpty()) return false;
+                stack.pop();
+                continue;
+            }
+            stack.add(s.charAt(i));
+        }
+
+        return stack.isEmpty();
+    }
 }
